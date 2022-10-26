67731d4404a68c7e58763cc8fd89afcf0b53fbee
Rayhan-Rony <jobayerahmedrony@gmail.com>
2022-10-27 01:49:06 +0600

    added routing

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 
-import './App.css';
+import { RouterProvider } from "react-router-dom";
+import { routes } from "./routes/Routes";
+
+
+
 
 function App() {
   return (
-    <div className="App">
+    <div>
 
+      <RouterProvider router={routes}></RouterProvider>
     </div>
   );
 }
---
+++ src/components/RightSideNav.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const RightSideNav = () => {
+    return (
+        <div>
+            <h1>This is aside</h1>
+        </div>
+    );
+};
+
+export default RightSideNav;
